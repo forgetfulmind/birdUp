1d39f111b69a81427cfe4f22ee9357665ce68ebd
osteophagy <mnovakahs@gmail.com>
2021-04-30 23:31:04 -0400

    post routes defined and mapped

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const postsController = require("../../controllers/postsController")
 const router = express.Router();
 //use Multer
 const uploadMulter = require('../../client/src/utils/middleWare/upload.js')
@@ -10,5 +11,8 @@ const { create } = require('../../controllers/postsController.js');
 
 
 router.post('/',uploadMulter, validation, create);
+router.get('/', postsController.findAll)
+router.get('/:id', postsController.findById)
+router.delete('/:id', postsController.remove)
 
 module.exports = router;
