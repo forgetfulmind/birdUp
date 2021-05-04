c75a9dae5bf99db2120687ebf3b0ce51dcab5f05
osteophagy <mnovakahs@gmail.com>
2021-05-04 14:13:34 -0400

    Fixedd small bug on edit profile, styling stuff + moving data around

--- client/src/components/UserPage/UserPage.js
+++ client/src/components/UserPage/UserPage.js
@@ -1,21 +1,53 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../Nav/index"
-import FeaturedObservation from "../FeaturedObservation/FeaturedObservation"
+import API from "../../utils/API";
+import { connect } from "react-redux";
 import style from "./style.css"
 
-function UserPage() {
+function UserPage({userId}) {
     // create state using hooks, drop info into placeholder locations
+    const [observationData, setObservationData] = useState([])
+
+    function loadPosts(){
+      API.getUserObservations(userId)
+      .then(observations => {
+        setObservationData(observations.data)
+      })
+    }
+
+    const [userProfile, setUserProfile] = useState([])
+
+    function loadUser() {
+      // console.log(userId)
+      API.findUser(userId)
+      .then(user => {
+          setUserProfile(user)
+          // console.log(user)
+      })
+    }
+
+    useEffect(()=>{
+      console.log(userProfile)
+    },[userProfile])
+
+    useEffect(()=>{
+      loadUser() 
+      loadPosts()
+      console.log(observationData)
+    },[])
+
     return (
     <div className="wrapper"> 
       <div className="mainImage d-flex justify-content-center align-items-center flex-column">
         <div className="filter">
          <div className="boxHeading">
-            <h1 className="heading">
-                Username
-            </h1>
+            <span className="heading">
+              { userProfile.length !== 0 ? <h1>{userProfile.data[0].username}</h1> : <p>BirdUp User</p> }
+            </span>
             <div className="userProps">
-                <p>Joined 02.21.21</p>
-                <p>00 Observations </p>
+              {userProfile.length !==0 ? <p>{userProfile.data[0].createdAt.split("T")[0]}</p> : <p>None</p>}
+              { observationData.length !==0 ? <p> {observationData.length} </p> : <p>Null</p> }
+              { userProfile.length !== 0 ? <img src={`/Images/2021${userProfile.data[0].profileimage.split(2021)[1]}`} className="observationImage"/> : <img />}
             </div>
           </div>
         </div>
@@ -27,4 +59,7 @@ function UserPage() {
   )
 }
 
-export default UserPage
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn, userId: state.auth.userId };
+  };
+  export default connect(mapStateToProps)(UserPage);
